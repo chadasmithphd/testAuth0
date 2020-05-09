815f88133762d1c5f4dcfa53e0d44650235727f9
chadasmithphd <@V7EDVT=7kzJ>
2020-05-09 15:31:56 -0600

    added ui

--- ui.R
+++ ui.R
@@ -1,9 +1,26 @@
-library(shiny)
-library(auth0)
+source('global.R', local = TRUE)
 
 # simple UI with user info
-ui <- fluidPage(
+ui <- auth0_ui(dashboardPage(
+  title = span("Dashboard"),
+  dashboardHeader(
+    title =  "Dashboard"
+    ),
+###################################     SIDEBAR    ################################################################
+
+dashboardSidebar(
+  sidebarMenu(
+    textOutput("Nickname"),
+    logoutButton(label = "Log out", id = "._auth0logout_")
+  )
+),
   
-  verbatimTextOutput("user_info"),
-  verbatimTextOutput("credential_info")
-)
+dashboardBody(
+  tabItem(tabName = "userInfo",
+          h2("This is for development only"),
+          textOutput("Sub"),
+          textOutput("name"),
+          textOutput("infoUpdated")
+  )
+  )
+  ), info = a0_info)
